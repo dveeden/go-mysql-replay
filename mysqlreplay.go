fd33b9f88e5f9c48ea2122246c29427c06d873e1
Daniël van Eeden <git@myname.nl>
2015-10-18 17:29:11 +0200

    Initial version

---
+++ mysqlreplay.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+        "strconv"
+        "time"
+)
+
+type ReplayStatement struct {
+	session int
+	epoch	float64
+	stmt	string
+}
+
+func mysqlsession(c <- chan ReplayStatement, session int, last_stmt_epoch float64) {
+	for {
+		pkt := <-c
+                if last_stmt_epoch != 0.0 {
+                	sleeptime := time.Duration(pkt.epoch - last_stmt_epoch) * time.Second
+                        fmt.Printf("Sleeptime: %s\n", sleeptime)
+                	time.Sleep(sleeptime)
+                }
+                last_stmt_epoch = pkt.epoch
+		fmt.Printf("STATEMENT REPLAY (session: %d): %s\n", session, pkt.stmt)
+	}
+}
+
+func main() {
+	fileflag := flag.String("f", "./test.dat", "Path to datafile for replay")
+	flag.Parse()
+
+	datFile, err := os.Open(*fileflag)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	reader := csv.NewReader(datFile)
+	reader.Comma = '\t'
+
+	pktData, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var startepoch float64 = 0.0
+        sessions := make(map[int]chan ReplayStatement)
+	for _, stmt := range pktData {
+                sessionid, err := strconv.Atoi(stmt[0])
+	        if err != nil {
+			fmt.Println(err)
+		}
+                epoch, err := strconv.ParseFloat(stmt[1], 64)
+	        if err != nil {
+			fmt.Println(err)
+		}
+                pkt := ReplayStatement{session: sessionid, epoch: epoch, stmt: stmt[2]}
+                if startepoch == 0.0 {
+			startepoch = pkt.epoch
+                }
+                if sessions[pkt.session] != nil {
+			sessions[pkt.session] <- pkt
+                } else {
+        		sess := make(chan ReplayStatement)
+        		sessions[pkt.session] = sess
+                        go mysqlsession(sessions[pkt.session], pkt.session, startepoch)
+			sessions[pkt.session] <- pkt
+                }
+	}
+}
